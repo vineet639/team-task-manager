a2dc0b11807728b32627db87d31616808ba8f1f0
Vineet Kumar <vinitsharma639@gmail.com>
2026-05-09 12:35:19 +0530

    updated config.py

--- backend/config.py
+++ backend/config.py
@@ -1,27 +1,15 @@
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 database_url = os.getenv("DATABASE_URL")
 
-if database_url:
-    database_url = database_url.replace(
-        "postgres://",
-        "postgresql://",
-        1
-    )
-
 class Config:
     SQLALCHEMY_DATABASE_URI = database_url
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SECRET_KEY = os.getenv(
-        "SECRET_KEY"
-    )
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
-    JWT_SECRET_KEY = os.getenv(
-        "JWT_SECRET_KEY"
-    )
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
