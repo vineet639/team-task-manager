b847e313fb363d467f5ddb56fe281d393f3554d1
Vineet Kumar <vinitsharma639@gmail.com>
2026-05-09 12:29:06 +0530

    Fix Railway PostgreSQL URL

--- backend/config.py
+++ backend/config.py
@@ -4,19 +4,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    database_url = database_url.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
 
 class Config:
-
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL"
-    )
+    SQLALCHEMY_DATABASE_URI = database_url
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    JWT_SECRET_KEY = os.getenv(
-        "JWT_SECRET_KEY"
-    )
-
     SECRET_KEY = os.getenv(
         "SECRET_KEY"
+    )
+
+    JWT_SECRET_KEY = os.getenv(
+        "JWT_SECRET_KEY"
     )
