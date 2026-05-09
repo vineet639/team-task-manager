52de8e054cf76dced64364db1ee30aaa6cbca8c8
Vineet Kumar <vinitsharma639@gmail.com>
2026-05-09 12:55:56 +0530

    updated api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,15 +1,22 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL,
 });
 
-api.interceptors.request.use((config => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-}));
+api.interceptors.request.use((config) => {
+
+  const token =
+    localStorage.getItem("token");
+
+  if (token) {
+
+    config.headers.Authorization =
+      `Bearer ${token}`;
+  }
+
+  return config;
+});
 
 export default api;
